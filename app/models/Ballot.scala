1bfe0e9fae866c8d35e4bcb8e53156229f04502d
Untoldwind <landru@untoldwind.net>
2014-03-07 14:07:20 +0100

    Mode app back to assets

--- app/models/Ballot.scala
+++ app/models/Ballot.scala
@@ -2,10 +2,10 @@ package models
 
 import play.api.db.slick.Config.driver.simple._
 
-case class Ballot(id: Int, moodlyId: String, cookieId: String, iterationCount: Int, vote: Int)
+case class Ballot(id: Long, moodlyId: String, cookieId: String, iterationCount: Int, vote: Int)
 
 class Ballots(tag: Tag) extends Table[Ballot](tag, "BALLOT") {
-  def id = column[Int]("ID", O.PrimaryKey, O.AutoInc)
+  def id = column[Long]("ID", O.PrimaryKey, O.AutoInc)
 
   def moodlyId = column[String]("MOODLY_ID", O.NotNull)
 
@@ -16,8 +16,14 @@ class Ballots(tag: Tag) extends Table[Ballot](tag, "BALLOT") {
   def vote = column[Int]("VOTE", O.NotNull)
 
   def * = (id, moodlyId, cookieId, iterationCount, vote) <>(Ballot.tupled, Ballot.unapply _)
+
+  def moodly = foreignKey("MOOLY_FK", moodlyId, Moodlies.moodlies)(_.id)
 }
 
 object Ballots {
   def ballots = TableQuery[Ballots]
+
+  def insert(ballot: Ballot)(implicit s: Session): Long = {
+    ballots.returning(ballots.map(_.id)).insert(ballot)
+  }
 }
