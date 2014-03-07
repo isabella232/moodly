c0c8dfda804047767290bd4e1b9494f8bc062028
notfb2 <fabian.bieker@leanovate.de>
2014-03-07 14:23:56 +0100

    GET /rest/moodlies

--- app/controllers/MoodlyResource.scala
+++ app/controllers/MoodlyResource.scala
@@ -1,7 +1,7 @@
 package controllers
 
 import play.api.mvc._
-import models.Moodlies
+import models.{Moodly, Moodlies}
 import play.api.db.slick._
 import play.api.Play.current
 import play.api.libs.json.Json
@@ -13,5 +13,13 @@ object MoodlyResource extends Controller {
       val moodly = new models.Moodly((request.body \ "intervalDays").as[Int])
       Moodlies.insert(moodly)
       Ok(Json.toJson(moodly))
+  }
+
+  def findById(id : String) = DBAction { implicit request =>
+    Moodlies.findById(id).map { moodly:Moodly =>
+      Ok(Json.toJson(moodly))
+    }.getOrElse {
+      NotFound(Json.toJson("Moodly not found"))
+    }
   }
 }
