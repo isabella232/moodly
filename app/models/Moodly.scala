d5a0cb2d4996377903162a7f77706caa0b8e12b6
Untoldwind <landru@untoldwind.net>
2014-03-07 12:48:50 +0100

    First DB insert

--- app/models/Moodly.scala
+++ app/models/Moodly.scala
@@ -3,9 +3,21 @@ package models
 import play.api.db.slick.Config.driver.simple._
 import java.sql.Timestamp
 import tyrex.services.UUID
+import play.api.libs.json.{JsNumber, JsString, JsObject, Writes}
 
 case class Moodly(id: String, start: Timestamp, intervalDays: Int) {
   def this(intervalDays: Int) = this(UUID.create(), new Timestamp(System.currentTimeMillis()), intervalDays)
+}
+
+object Moodly {
+
+  implicit object MoodlyWrites extends Writes[Moodly] {
+    override def writes(moodly: Moodly) = JsObject(Seq(
+      "id" -> JsString(moodly.id),
+      "start" -> JsNumber(moodly.start.getTime),
+      "intervalDays" -> JsNumber(moodly.intervalDays)))
+  }
+
 }
 
 class Moodlies(tag: Tag) extends Table[Moodly](tag, "MOODLY") {
@@ -15,7 +27,9 @@ class Moodlies(tag: Tag) extends Table[Moodly](tag, "MOODLY") {
 
   def intervalDays = column[Int]("INTERVAL_DAYS", O.NotNull)
 
-  def * = (id, start, intervalDays) <>(Moodly.tupled, Moodly.unapply _)
+  def * = (id, start, intervalDays) <>( {
+    row: (String, Timestamp, Int) => Moodly(row._1, row._2, row._3)
+  }, Moodly.unapply _)
 }
 
 object Moodlies {
