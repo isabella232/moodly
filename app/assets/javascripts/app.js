b7cc9a9b75accd43fbf850a8fe4e2ed1d1bd7ab3
teewitt <info@trickybits.eu>
2014-04-04 19:07:09 +0200

    - story auswertung

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -21,60 +21,86 @@ require([ 'angular', 'angular-route', 'angular-resource', 'angular-cookies'], fu
         };
     }
 
-    function VotingCtrl($scope, $routeParams, $resource, $cookieStore, $window) {
+    function VotingCtrl($scope, $routeParams, $resource, $cookieStore, $window, CookieService) {
 
         $resource('/rest/moodlies/:moodlyId/currentIterationCount', {moodlyId:$routeParams.id}).get(function(data) {
             console.log("currentIterationCount=" + data[0]);
-            var cookieKey = "moodly-" + $routeParams.id + "-" + data[0];
-
-            if ($cookieStore.get(cookieKey)) {
+            var cookieKey = "moodly-" + $routeParams.id;
+            var cookie = ($cookieStore.get(cookieKey)) ? JSON.parse($cookieStore.get(cookieKey)) : {};
+            console.log(cookie);
+            if (cookie.currentIterationCount == data[0]) {
                 console.log("user has cookie!");
-                $window.location.hash = "/voted/" + $routeParams.id + "/TODO";
+                $window.location.hash = "/voted/" + $routeParams.id;
             }
 
             // TODO: this is a minor race condition
             $scope.vote = function(ballot) {
+                var cookieId = Math.random();
                 var Ballot = $resource('/rest/moodlies/:moodlyId/ballots', {moodlyId:$routeParams.id},{});
-                if (!$cookieStore.get(cookieKey)) {
-                    $cookieStore.put(cookieKey, Math.random());
-                }
-                Ballot.save({cookieId:$cookieStore.get(cookieKey), vote:ballot});
+
+                CookieService.put($routeParams.id, {cookieId:cookieId, ballot:ballot, currentIterationCount:parseInt(data[0], 10)});
+
+                Ballot.save({cookieId:cookieId.toString(), vote:ballot});
                 console.log(ballot);
-                $window.location.hash = "/voted/" + $routeParams.id + "/" + ballot;
+                $window.location.hash = "/voted/" + $routeParams.id;
             };
         });
 
     }
 
-    function VotedCtrl($scope, $routeParams, $resource) {
-        $scope.ballot = $routeParams.ballot
-//        var Ballot = $resource('/rest/moodlies/:moodlyId/ballots', {moodlyId:$routeParams.id},
-//                {
-//                    get : {
-//                      method: 'GET',
-//                      isArray: true
-//                    }
-//                }
-//        );
-//        Ballot.get();
+    function VotedCtrl($scope, $routeParams, $resource, CookieService) {
+        $scope.ballot = CookieService.get($routeParams.id).ballot;
+
+        var Ballot = $resource('/rest/moodlies/:moodlyId/ballots', {moodlyId:$routeParams.id},
+                {
+                    get : {
+                      method: 'GET',
+                      isArray: true
+                    }
+                }
+        );
+
+        Ballot.get(function(data) {
+            var cIC = CookieService.get($routeParams.id).currentIterationCount;
+            console.log(data);
+            $scope.avg = Math.round((data.reduce(function(acc, b) {
+                return acc + (b.iterationCount == cIC ? b.vote : 0)
+            }, 0) / data.length) * 100) / 100;
+        });
     }
 
-    angular.module('moodly', ['ngRoute', 'ngResource', 'ngCookies' ]).
-        config(['$routeProvider', function ($routeProvider) {
-            $routeProvider.when('/', {
-                templateUrl: 'partials/moodlies.html',
-                controller: MoodlyCtrl,
-                reloadOnSearch: false
-            }).when('/voting/:id', {
-                templateUrl: 'partials/voting.html',
-                controller: VotingCtrl,
-                reloadOnSearch: false
-            }).when('/voted/:id/:ballot', {
-                        templateUrl: 'partials/voted.html',
-                        controller: VotedCtrl,
-                        reloadOnSearch: false
-            }).otherwise({redirectTo: '/'});
-        }]);
+    angular.module('moodly', ['ngRoute', 'ngResource', 'ngCookies' ]);
+
+    angular.module('moodly').service('CookieService', function($cookieStore) {
+        this.get = function(cookieName) {
+            var cookieKey = "moodly-" + cookieName;
+            return ($cookieStore.get(cookieKey)) ? JSON.parse($cookieStore.get(cookieKey)) : {};
+        };
+
+        this.put = function(cookieName, data) {
+            var cookieKey = "moodly-" + cookieName;
+            $cookieStore.put(cookieKey, JSON.stringify(data));
+        };
+    });
+
+    angular.module('moodly').config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/', {
+            templateUrl: 'partials/moodlies.html',
+            controller: MoodlyCtrl,
+            reloadOnSearch: false
+        }).when('/voting/:id', {
+            templateUrl: 'partials/voting.html',
+            controller: VotingCtrl,
+            reloadOnSearch: false
+        }).when('/voted/:id', {
+            templateUrl: 'partials/voted.html',
+            controller: VotedCtrl,
+            reloadOnSearch: false
+        }).otherwise({redirectTo: '/'});
+    }]);
 
     angular.bootstrap(document, ['moodly']);
+
+
 });
+
