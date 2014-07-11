f29af0c71fd0f536bed8b20a9254f771e453506b
Yann Simon <yann.simon.fr@gmail.com>
2014-07-11 12:37:18 +0200

    add the possibility to test locally with a postgres database

---
+++ withpostgres.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# start the application with a local postgres database
+# the local postges database must exit
+
+# to create this database:
+# sudo su postgres -c psql
+# create user moodly with password 'moodly';
+# create database moodly;
+# grant all privileges on database moodly to moodly;
+
+sbt \
+-Ddb.default.driver=org.postgresql.Driver \
+-Ddb.default.url="jdbc:postgresql://127.0.0.1:5432/moodly" \
+-Ddb.default.user=moodly \
+-Ddb.default.password="moodly"
